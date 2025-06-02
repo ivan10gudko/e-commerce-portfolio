516c7e240e94e9005f4d04655d2a8c8149133236
Hudko Ivan <giv.10072007@gmail.com>
2025-06-02 21:21:46 +0300

    fix UI footer(font family and traking width)

--- src/ui/CheckoutFooter.jsx
+++ src/ui/CheckoutFooter.jsx
@@ -1,7 +1,7 @@
 function CheckoutFooter() {
   return (
     <footer>
-      <ul className="flex w-full border-t border-black/60 text-sm gap-4 mt-20 items-center py-8 underline ">
+      <ul className="flex w-full border-t border-black/60 text-sm gap-4 mt-20 items-center py-8 font-urbanist tracking-wide underline ">
         <li>Refund policy</li>
         <li>Privacy policy</li>
         <li>Terms of service</li>
